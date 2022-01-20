afb04db6c2ccb21f96811a51a4d7c51ddcb0fe72
Decristony <decristony@gmail.com>
2022-01-20 16:43:18 -0300

    Update carrossel

---
+++ script/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 3
+    },
+    600: {
+      items: 4
+    },
+    800: {
+      items: 5
+    },
+    1000: {
+      items: 7
+    },
+    1500: {
+      items: 8
+    }
+  }
+})
